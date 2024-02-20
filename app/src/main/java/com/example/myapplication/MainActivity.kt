88edca073971a2b8ec417257e389db0d255872dd
Alexandria Macko-Millard <89824931+AlexandriaMM@users.noreply.github.com>
2024-02-20 15:01:38 -0500

    Fixing Merge Conflicts

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,13 @@ package com.example.myapplication
 
 import android.R
 import android.os.Bundle
-<<<<<<< HEAD
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
-
-=======
 import java.util.Timer
->>>>>>> 02e2734103f6f3bfc9f6e3a5ba8ba9deb566e14c
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +18,18 @@ class MainActivity : AppCompatActivity() {
         ServiceClient.initQueue(this)
     }
 
-<<<<<<< HEAD
     public fun buttonTapped(view: View) {
-        val stockSymbol = findViewById<TextView>(R.id.stockInputText).text.toString()
+        val stockSymbol = findViewById<EditText>(R.id.stockInputText).text.toString()
         val jsonObject = JSONObject()
-        val myButton =
 
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object:TimerTask(){
+            override fun run() {
+                ServiceClient.sendRequest(jsonRequest)
+            }
+        }, 0, 5000)
 
         jsonObject.get("stockSymbol," stockSymbol)
     }
-=======
-    val timer = Timer()
-    timer.scheduleAtFixedRate(object:TimerTask(){
-        override fun run() {
-            ServiceClient.sendRequest(jsonRequest)
-        }
-    }, 0, 5000)
 
->>>>>>> 02e2734103f6f3bfc9f6e3a5ba8ba9deb566e14c
 }
