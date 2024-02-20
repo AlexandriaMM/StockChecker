dd0a502b9b2ea2d3d2a5c67d9a6575ed83db3ff1
Alexandria Macko-Millard <89824931+AlexandriaMM@users.noreply.github.com>
2024-02-20 16:33:13 -0500

    Added stuff to Main Activty
    For some reason the request is not fully firing off, it seems to get stuck

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.myapplication
-
-import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonRequest
 import org.json.JSONObject
-import java.util.Timer
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,17 +20,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     public fun buttonTapped(view: View) {
-        val stockSymbol = findViewById<EditText>(R.id.stockInputText).text.toString()
+        val stockSymbol = findViewById<EditText>(R.id.stockInputText)
+        val stockSymbolText = stockSymbol.text.toString()
         val jsonObject = JSONObject()
-
+        jsonObject.put("stockSymbol", stockSymbolText)
         val timer = Timer()
         timer.scheduleAtFixedRate(object:TimerTask(){
             override fun run() {
-                ServiceClient.sendRequest(jsonRequest)
+                ServiceClient.get(jsonObject,
+                    Response.Listener {
+                        Log.i("stockPrice", it.getString("stockPrice"))
+                        runOnUiThread{
+                            stockSymbol.setText(it.getString("stockPrice"))
+                        }
+                    },
+                    Response.ErrorListener {
+                        Log.e("stockPrice", it.message.toString())
+                    }
+                )
             }
         }, 0, 5000)
-
-        jsonObject.get("stockSymbol," stockSymbol)
     }
-
 }
