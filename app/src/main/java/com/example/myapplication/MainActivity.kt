7f0b3f6a896b53edceac6767d81834451ef760da
viviansomes <vsomes20@bw.edu>
2024-02-20 14:51:00 -0500

    Update MainActivity.kt
    added button tapped, still has error :(

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ServiceClient.initQueue(this)
     }
 
+    public fun buttonTapped(view: View) {
+        val stockSymbol = findViewById<TextView>(R.id.stockInputText).text.toString()
+        val jsonObject = JSONObject()
+        val myButton =
+
+
+        jsonObject.get("stockSymbol," stockSymbol)
+    }
 }
